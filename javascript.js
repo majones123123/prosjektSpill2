0240ea9c6ea699e9e2e7d0af38757813ec387672
majones123123 <jonstangmathias@gmail.com>
2025-03-02 13:26:28 +0100

    Fullført spill

--- javascript.js
+++ javascript.js
@@ -1,33 +1,6 @@
-const canvas = document.getElementById("myCanvas");
+
 
 let platformElm = document.querySelector(".platform");
-
-// function platformSize(HEIGHT, WIDTH) {};
-
-// Høyde og bredde på skjermen
-const HEIGHT = 500;
-const WIDTH = 500;
-const canvasElm = document.querySelector("canvas");
-canvasElm.height = HEIGHT;
-canvasElm.width = WIDTH;
-const ctx = canvasElm.getContext("2d");
-
-//Platform plassering
-function renderplat() {
-  ctx.fillStyle = "#45597E";
-  ctx.fillRect(
-    platforms[0].x,
-    platforms[0].y,
-    platforms[0].width,
-    platforms[0].height
-  );
-  ctx.fillRect(
-    platforms[1].x,
-    platforms[1].y,
-    platforms[1].width,
-    platforms[1].height
-  );
-}
 
 //Spilleren
 let player = {
@@ -190,3 +163,14 @@ function loop2() {
   createplat(10);
 }
 
+//Starter spillet
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    ctx.canvas.height = 500;
+    ctx.canvas.width = 800;
+    
+    
+    document.addEventListener("keydown", keydown);
+    document.addEventListener("keyup", keyup);
+    setInterval(loop, 22);
+    setInterval(loop2, 2000)
